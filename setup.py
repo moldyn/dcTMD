d30c1b7d3b848607e053f4de0fb96644f3d17b78
braniii <daniel.nagel@protonmail.com>
2023-01-03 11:48:41 +0100

    Add project urls to setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     name='dcTMD',
     version='0.2.1',
     description='Analyse targeted molecular dynamics data with dcTMD',
-    #long_description=README,
+    long_description=README,
     keywords=[
         'enhance sampling',
         'friction',
@@ -45,15 +45,15 @@ setup(
     ],
     author='taenzel, dieJaegerIn, floWneffetS',
     project_urls={
-        #'Documentation': 'https://moldyn.github.io/MoSAIC',
+        'Documentation': 'https://moldyn.github.io/dcTMD',
         'Source Code': 'https://github.com/moldyn/dcTMD',
-        #'Bug Tracker': 'https://github.com/moldyn/MoSAIC/issues',
+        'Bug Tracker': 'https://github.com/moldyn/dcTMD/issues',
     },
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     include_package_data=True,
     python_requires='>=3.8',
-        install_requires=[
+    install_requires=[
         'numpy>=1.21.0',
         'matplotlib',
         'beartype>=0.10.4',
