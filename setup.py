7802eefad47ab3adac571f0aa7c3d641cbac6765
braniii <daniel.nagel@protonmail.com>
2023-01-03 11:48:19 +0100

    Add extra requirements for testing and docs.

--- setup.py
+++ setup.py
@@ -1,4 +1,37 @@
+# -*- coding: utf-8 -*-
+import pathlib
+from collections import defaultdict
 from setuptools import setup, find_packages
+
+
+def get_extra_requirements(path, add_all=True):
+    """Parse extra-requirements file."""
+    with open(path) as depfile:
+        extra_deps = defaultdict(set)
+        for line in depfile:
+            if not line.startswith('#'):
+                if ':' not in line:
+                    raise ValueError(
+                        f'Dependency in {path} not correct formatted: {line}',
+                    )
+                dep, tags = line.split(':')
+                tags = {tag.strip() for tag in tags.split(',')}
+                for tag in tags:
+                    extra_deps[tag].add(dep)
+
+        # add tag `all` at the end
+        if add_all:
+            extra_deps['all'] = {
+                tag for tags in extra_deps.values() for tag in tags
+            }
+
+    return extra_deps
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+README = (HERE / 'README.md').read_text()
 
 setup(
     name='dcTMD',
@@ -29,6 +62,7 @@ setup(
         'click>=7.0.0',
         'typing_extensions>=3.9.0;python_version<"3.9"',
     ],
+    extras_require=get_extra_requirements('extra-requirements.txt'),
     entry_points={
         'console_scripts': [
             'dcTMD = dcTMD.__main__:main',
