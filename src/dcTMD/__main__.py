5c2f649b9041427e0248aa1024c00ee4243943ec
dieJaegerIn <miriam.jaeger@physik.uni-freiburg.de>
2023-03-08 11:37:50 +0100

    linting, delted errors and got rid of Mutex

--- src/dcTMD/__main__.py
+++ src/dcTMD/__main__.py
@@ -10,10 +10,6 @@ All rights reserved.
 import click
 import dcTMD
 
-import numpy as np
-
-from dcTMD._clickextensions import Mutex
-
 MODES = ('work', 'force')
 
 
@@ -34,7 +30,8 @@ MODES = ('work', 'force')
     'pullf_files',
     required=True,
     help='Input: File containing list of all constraint force file names' +
-    'or glob pattern e.g.'*.xvg' to generate a list of all constraint force files using glob.glob()',
+    'or glob pattern e.g."*.xvg" to generate a list of all constraint ' +
+    'force files using glob.glob()',
 )
 @click.option(
     '-o',
@@ -73,11 +70,11 @@ MODES = ('work', 'force')
     help='Standard deviation of gaussian filter in nm.',
 )
 @click.option(
-    '-N',
-    '--N_resamples',
-    type=float,
+    '--resamples',
+    type=int,
     required=False,
-    help='Number of resamples used in optional bootstrapping. This is only available in mode work',
+    help='Number of resamples used in optional bootstrapping. ' +
+    'This is only available in mode work',
 )
 @click.option(
     '-v',
@@ -88,7 +85,7 @@ MODES = ('work', 'force')
     help='Enable verbose mode.',
 )
 @click.option(
-    'p',
+    '-p',
     '--plot',
     is_flag=True,
     default=False,
@@ -96,7 +93,7 @@ MODES = ('work', 'force')
     help='Plots free energy and smoothed friction.',
 )
 @click.option(
-    '-sd'
+    '-sd',
     '--save_dataset',
     is_flag=True,
     default=False,
@@ -111,7 +108,7 @@ def main(
     velocity,
     res,
     sigma,
-    N_resamples,
+    resamples,
     verbose,
     plot,
     save_dataset,
@@ -133,48 +130,48 @@ def main(
     6182.
     """
     # Click aftercare
+    # ToDo: discuss if we want to include error estimation
     if verbose:
         click.echo(f'Input:\n mode: {mode}\n file: {pullf_files}\n \
         outname: {outname}\n temperature: {temperature}\n \
         velocity: {velocity}\n resolution: {res}\n sigma: {sigma}\n verbose: \
-        {verbose}\n, plot: {plot}\n, N_resamples: {N_resamples}\n, \
+        {verbose}\n, plot: {plot}\n, N_resamples: {resamples}\n\
         save dataset: {save_dataset}\n')
-
-    if not (pullf_files or pullf_glob_pattern):
-        print("Please provide constraint force files via '-f' or '-g'.")
-        exit()
 
     # Set up mode
     if mode == 'work':
-        dataset = dcTMD.storing.WorkSet(velocity=velocity, 
-                                              resolution=res,
-                                              )
-        estimator = dcTMD.dcTMD.ForceEstimator(temperature)
+        dataset = dcTMD.storing.WorkSet(velocity=velocity,
+                                        resolution=res,
+                                        )
+        estimator = dcTMD.dcTMD.WorkEstimator(temperature)
         outname += '_from_work'
     if mode == 'force':
-        dataset_class = dcTMD.storing.ForceSet(velocity=velocity, 
-                                              resolution=res,
-                                              )
-        estimator = dcTMD.dcTMD.WorkEstimator(temperature)
+        dataset = dcTMD.storing.ForceSet(velocity=velocity,
+                                         resolution=res,
+                                         )
+        estimator = dcTMD.dcTMD.ForceEstimator(temperature)
         outname += '_from_forceacf'
 
+    print(dataset, estimator)
     # Loading constraint force files
     filenames = dcTMD.io.load_pullf(pullf_files)
 
     # Generate work/force set
     dataset.fit(filenames)
     if save_dataset:
-        out = outname + f"_{len(dataset.names_)}" 
+        out = outname + f"_{len(dataset.names_)}"
         dcTMD.storing.save(dataset, out)
     # Calculate Wmean, Wdiss, dG and friction factor
     estimator.fit(dataset)
-    
+
+    """
     # calculate errors
-    if N_resamples:
+    if resamples:
         estimator.estimate_free_energy_errors(N_resamples)
+    """
 
     # save data as .npz and .dat file
-    dcTMD.io.write_output(dataset, estimator)
+    dcTMD.io.write_output(outname, dataset, estimator)
 
 
 if __name__ == '__main__':
