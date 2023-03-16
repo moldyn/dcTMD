e904688a268904adf6d2668e4d12637ededc3890
dieJaegerIn <miriam.jaeger@physik.uni-freiburg.de>
2023-03-16 10:14:35 +0100

    WIP: implementing plotting.py

--- src/dcTMD/utils/__init__.py
+++ src/dcTMD/utils/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ['smoothing', 'bootstrapping', 'dcTMDplots']
+__all__ = ['smoothing', 'bootstrapping', 'plotting']
 
 from dcTMD.utils import smoothing
 from dcTMD.utils import bootstrapping
-from dcTMD.utils import dcTMDplots
+from dcTMD.utils import plotting
 
 __version__ = '0.2.2'
--- src/dcTMD/utils/dcTMDplots.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Simple plot functions for dcTMD results.
-
-MIT License
-Copyright (c) 2021-2022, Miriam Jäger
-All rights reserved.
-"""
-
