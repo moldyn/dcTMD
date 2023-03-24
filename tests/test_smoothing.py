bec0705bb8043eb90a60e7afae031b87308976f7
braniii <daniel.nagel@protonmail.com>
2023-03-24 10:46:39 +0100

    Fix typo, pep8 errors and test against smoothed friction result.

--- tests/test_smoothing.py
+++ tests/test_smoothing.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
-"""
-Tests for the smoothing module.
+"""Tests for the smoothing module.
 
 MIT License
-Copyright (c) 2021-2022, Miriam Jäger
+Copyright (c) 2021-2023, Miriam Jäger
 All rights reserved.
 """
 
 import pytest
 import numpy as np
-from numpy.testing import assert_allclose
-import matplotlib.pyplot as plt
 from os.path import dirname, join
 from dcTMD.dcTMD import WorkEstimator
 from dcTMD.storing import load
@@ -23,20 +20,25 @@ VERBOSE = True
 TEMPERATURE = 300
 INDICES = np.array([1, 3, 5])
 HERE = dirname(__file__)
-TEST_FILE_DIR = join(HERE, 'testdata')
+TESTFILE_DIR = join(HERE, 'testdata')
 
 
 @pytest.fixture
 def ref_workestimator(scope="session"):
-    workset_name = f'{TEST_FILE_DIR}/workset'
+    workset_name = join(TESTFILE_DIR, 'workset')
     workset = load(filename=workset_name)
     estimator = WorkEstimator(temperature=TEMPERATURE)
     return estimator.fit(workset)
 
 
-def test_gaussfilter_friction(ref_workestimator):
-    smooth_friction_ = gaussfilter_friction(ref_workestimator.friction_,
-                                            ref_workestimator.position_,
-                                            0.1,
-                                            )
-    
+@pytest.fixture
+def ref_friction(scope="session"):
+    filename = join(TESTFILE_DIR, 'workset.smoothed_friction')
+    return np.loadtxt(filename)
+
+
+def test_gaussfilter_friction(ref_workestimator, ref_friction):
+    smooth_friction = gaussfilter_friction(
+        ref_workestimator.friction_, ref_workestimator.position_, 0.1,
+    )
+    np.testing.assert_array_almost_equal(smooth_friction, ref_friction)
